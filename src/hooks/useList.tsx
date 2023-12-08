e6324dd25d2c8ac7ac2dff23a285577a58717c45
Evanpatchouli <3125019716@qq.com>
2023-12-08 18:49:48 +0800

    useList : plus+

--- src/hooks/useList.tsx
+++ src/hooks/useList.tsx
@@ -1,55 +1,114 @@
+import { UniqueKey } from "@/utils";
 import { useState, useEffect, useCallback } from "react";
 
-type Item = { [key: string]: any };
-type SortFunction = (a: Item, b: Item) => number;
-type FilterFunction = (item: Item) => boolean;
-type RenderFunction = (item: Item) => JSX.Element;
+type Item<T extends object = {}> = T;
+type ItemExtended<T extends object = {}> = Item<T> & {
+  _id: any;
+};
+type SortFunction<T extends object = {}> = (a: Item, b: ItemExtended<T>) => number;
+type FilterFunction<T extends object = {}> = (item: ItemExtended<T>) => boolean;
+type RenderFunction<T extends object = {}> = (item: ItemExtended<T>) => JSX.Element;
 
-interface UseListOptions {
+interface UseListOptions<T extends object = {}> {
   idKey?: string;
-  sortFn?: SortFunction;
-  filterFn?: FilterFunction;
-  renderFn?: RenderFunction;
+  sortFn?: SortFunction<T>;
+  filterFn?: FilterFunction<T>;
+  renderFn?: RenderFunction<T>;
 }
 
-function useList(
-  initialItems: Item[],
+function useList<T extends object = {}>(
+  initialItems: Item<T>[],
   dependencies: any[],
-  options: UseListOptions
+  options: UseListOptions<T>
 ): [
-  Item[],
-  (newItems: Item[]) => void,
-  (item: Item) => void,
-  () => void,
-  Item[]
+  Item<T>[],
+  {
+    readonly updateItems: (newItems: Item<T>[]) => void;
+    readonly addItem: (item: Item<T>) => void;
+    readonly removeItem: (id: string | number) => void;
+    readonly save: () => void;
+    readonly reset: () => void;
+    readonly filteredItems: Item<T>[];
+    readonly originalItems: Item<T>[];
+    readonly render: () => React.ReactNode;
+  }
 ] {
-  const [items, setItems] = useState<Item[]>([...initialItems]);
-  const [originalItems, setOriginalItems] = useState<Item[]>([...initialItems]);
+  const [items, setItems] = useState<ItemExtended<T>[]>(
+    // @ts-ignore
+    [...initialItems].map((item) => ({ ...item, [options.idKey || "_id"]: UniqueKey() }))
+  );
+  const [originalItems, setOriginalItems] = useState<Item<T>[]>([...initialItems]);
 
   useEffect(() => {
-    setOriginalItems([...items]);
+    // 去除 唯一id 再设置
+    const newItems = items.map((item) => {
+      const _item = { ...item };
+      if (_item[options.idKey || "_id"]) {
+        delete _item[options.idKey || "_id"];
+      }
+
+      return _item;
+    });
+    setOriginalItems([...newItems]);
   }, dependencies);
 
   const save = useCallback(() => {
-    setOriginalItems([...items]);
+    const newItems = items.map((item) => {
+      const _item = { ...item };
+      if (_item[options.idKey || "_id"]) {
+        delete _item[options.idKey || "_id"];
+      }
+
+      return _item;
+    });
+    setOriginalItems([...newItems]);
   }, [items]);
 
   const addItem = useCallback(
     (item: Item) => {
-      setItems((prevItems) => [
-        ...prevItems,
-        { ...item, [options.idKey || "_id"]: Date.now().toString() },
-      ]);
+      // @ts-ignore
+      setItems((prevItems) => [...prevItems, { ...item, [options.idKey || "_id"]: UniqueKey() }]);
     },
     [options.idKey]
   );
 
-  const updateItems = useCallback((newItems: Item[]) => {
+  const removeItem = useCallback(
+    (id: string | number) => {
+      setItems((prevItems) => prevItems.filter((item) => item[options.idKey || "_id"] !== id));
+    },
+    [options.idKey]
+  );
+
+  const reset = useCallback(() => {
+    // @ts-ignore
+    setItems([...originalItems]);
+  }, [originalItems]);
+
+  const updateItems = useCallback((newItems: Item<T>[]) => {
+    // @ts-ignore
     setItems(newItems);
   }, []);
 
   const sortedItems = [...items].sort(options.sortFn || (() => 0));
   const filteredItems = sortedItems.filter(options.filterFn || (() => true));
 
-  return [items, updateItems, addItem, save, filteredItems];
+  return [
+    items,
+    {
+      updateItems,
+      addItem,
+      removeItem,
+      save,
+      reset,
+      filteredItems,
+      originalItems,
+      render: () => {
+        return filteredItems.map((item) => {
+          return options.renderFn ? options.renderFn(item) : null;
+        });
+      },
+    } as const,
+  ];
 }
+
+export default useList;
