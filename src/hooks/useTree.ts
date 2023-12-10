cd133d6dbc1e922a26a8a6cb5970398b54f1092a
evanpatchouli <3125019716@qq.com>
2023-12-11 01:41:37 +0800

    hook : useTree

--- src/hooks/useTree.ts
+++ src/hooks/useTree.ts
@@ -1,55 +1,141 @@
-import { useState, useEffect, useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
-type Item = { [key: string]: any };
-type SortFunction = (a: Item, b: Item) => number;
-type FilterFunction = (item: Item) => boolean;
-type RenderFunction = (item: Item) => JSX.Element;
+type TreeNode<K extends string | number = "_id"> = {
+  // @ts-ignore
+  [idKey: K]: any;
+  children: TreeNode[];
+  [key: string]: any;
+};
 
-interface UseListOptions {
-  idKey?: string;
-  sortFn?: SortFunction;
-  filterFn?: FilterFunction;
-  renderFn?: RenderFunction;
+interface UseTreeOptions {
+  idKey?: string | number;
+  renderNode?: (node: TreeNode) => React.JSX.Element | React.ReactNode;
+  filterFn?: (node: TreeNode) => boolean;
 }
 
-function useList(
-  initialItems: Item[],
-  dependencies: any[],
-  options: UseListOptions
-): [
-  Item[],
-  (newItems: Item[]) => void,
-  (item: Item) => void,
-  () => void,
-  Item[]
-] {
-  const [items, setItems] = useState<Item[]>([...initialItems]);
-  const [originalItems, setOriginalItems] = useState<Item[]>([...initialItems]);
+const useTree = (
+  initialTree: TreeNode,
+  options: UseTreeOptions = { idKey: "_id" }
+) => {
+  const [tree, setTree] = useState<TreeNode>(initialTree);
+  const [filteredTree, setFilteredTree] = useState<TreeNode | null>(null);
+  const idKey = options.idKey;
+  const renderNode = options.renderNode || (() => null);
+  const filterFn = options.filterFn;
+
+  if (!idKey && idKey !== 0) {
+    throw new Error("You must provide an idKey to useTree");
+  }
+
+  const traverse = (node: TreeNode, callback: (node: TreeNode) => any) => {
+    const result = callback(node);
+    const childrenResults: any = node.children.map((child) =>
+      traverse(child, callback)
+    );
+    const final = [result, ...childrenResults];
+
+    return final as any[];
+  };
+
+  const addNode = (node: TreeNode, parentId: string) => {
+    traverse(tree, (currentNode) => {
+      if (currentNode[idKey] === parentId) {
+        currentNode.children.push(node);
+      }
+    });
+    setTree({ ...tree });
+  };
+
+  const removeNode = (nodeId: string) => {
+    traverse(tree, (currentNode) => {
+      currentNode.children = currentNode.children.filter(
+        (child) => child[idKey] !== nodeId
+      );
+    });
+    setTree({ ...tree });
+  };
+
+  const updateNode = (nodeId: string, newNodeData: any) => {
+    traverse(tree, (currentNode) => {
+      if (currentNode[idKey] === nodeId) {
+        Object.assign(currentNode, newNodeData);
+      }
+    });
+    setTree({ ...tree });
+  };
+
+  const findNode = (nodeId: string): TreeNode | null => {
+    let foundNode = null;
+    traverse(tree, (currentNode) => {
+      if (currentNode[idKey] === nodeId) {
+        foundNode = currentNode;
+      }
+    });
+    return foundNode;
+  };
+
+  const searchTree = (filter: string | ((node: TreeNode) => boolean)) => {
+    let filterFn: any;
+
+    if (typeof filter === "string") {
+      filterFn = (node: TreeNode) => node[idKey] === filter;
+    } else {
+      filterFn = filter;
+    }
+
+    const result = traverse(tree, (node: TreeNode) => {
+      if (filterFn(node)) {
+        return node;
+      } else {
+        return null;
+      }
+    });
+
+    return result.filter((node: TreeNode) => node !== null);
+  };
+
+  const moveNode = (sourceNodeId: string, targetNodeId: string) => {
+    let sourceNode = null;
+    traverse(tree, (currentNode) => {
+      if (currentNode[idKey] === sourceNodeId) {
+        sourceNode = currentNode;
+      }
+    });
+
+    if (sourceNode === null) {
+      throw new Error(`Node with id ${sourceNodeId} does not exist`);
+    }
+
+    removeNode(sourceNodeId);
+
+    addNode(sourceNode, targetNodeId);
+  };
+
+  const render = useCallback(() => {
+    if (!renderNode) {
+      throw new Error("You must provide a renderNode function to useTree");
+    }
+    return traverse(tree, renderNode);
+  }, [tree, renderNode]);
 
   useEffect(() => {
-    setOriginalItems([...items]);
-  }, dependencies);
+    if (filterFn) {
+      const result = traverse(tree, (node: TreeNode) => {
+        if (filterFn(node)) {
+          return node;
+        } else {
+          return null;
+        }
+      });
+      // @ts-ignore
+      setFilteredTree(result.filter((node: TreeNode) => node !== null));
+    }
+  }, [tree, filterFn]);
 
-  const save = useCallback(() => {
-    setOriginalItems([...items]);
-  }, [items]);
+  return [
+    tree,
+    { addNode, removeNode, updateNode, findNode, moveNode, searchTree, render },
+  ];
+};
 
-  const addItem = useCallback(
-    (item: Item) => {
-      setItems((prevItems) => [
-        ...prevItems,
-        { ...item, [options.idKey || "_id"]: Date.now().toString() },
-      ]);
-    },
-    [options.idKey]
-  );
-
-  const updateItems = useCallback((newItems: Item[]) => {
-    setItems(newItems);
-  }, []);
-
-  const sortedItems = [...items].sort(options.sortFn || (() => 0));
-  const filteredItems = sortedItems.filter(options.filterFn || (() => true));
-
-  return [items, updateItems, addItem, save, filteredItems];
-}
+export default useTree;
