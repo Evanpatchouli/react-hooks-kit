1f87d6b24fe59b82268dd22b88146a39d84cd9e7
evanpatchouli <3125019716@qq.com>
2023-12-18 23:24:15 +0800

    update : linkTo

--- src/utils/linkTo.ts
+++ src/utils/linkTo.ts
@@ -1,14 +1,19 @@
-const linkTo = (path?: string | { path?: string; hash?: boolean }, hash?: boolean) => {
+const linkTo = (
+  path?: string | { path?: string; hash?: boolean },
+  hash?: boolean
+) => {
   if (path) {
     if (typeof path === "string" && path) {
       if (window.location.hash || hash) {
-        window.location.hash = path;
+        window.location.hash = path.startsWith("#/") ? path : "#/" + path;
       } else {
         window.location.assign(path);
       }
     } else if (typeof path === "object" && path.path) {
       if (window.location.hash || (path.hash ?? hash)) {
-        window.location.hash = path.path;
+        window.location.hash = path.path.startsWith("#/")
+          ? path.path
+          : "#/" + path.path;
       } else {
         window.location.assign(path.path);
       }
