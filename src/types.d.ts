d51cf4572432508ae3327a409c2f7f5a822ee4dd
Evanpatchouli <evanpatchouli@foxmail.com>
2026-08-18 15:59:34 +0800

    fix(types): 声明 SVG 模块

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,10 @@
 declare module "*.css" {}
 
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
 declare module "*.mdx" {
   let MDXComponent: (props: any) => JSX.Element;
   export default MDXComponent;
